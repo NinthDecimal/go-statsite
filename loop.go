23f7f1d69134ae3f80e0bc8f7941acf3fb5211ae
Stephen Hitchner <stephen@kiip.me>
2015-08-27 11:49:06 -0700

    adding metric helper methods and main flush loop

---
+++ loop.go
@@ -0,0 +1,63 @@
+package statsite
+
+import (
+	"log"
+	"time"
+)
+
+// Whether to enable StatSiteMetrics
+var enabled bool = false
+
+// Time to wait on error
+var ErrorWaitTime time.Duration = time.Duration(10 * time.Second)
+
+// Metric Prefix
+var metricPrefix string
+
+var statQueue chan Message
+
+func Initialize(hostname string, prefix string) {
+	enabled = true
+	statQueue = make(chan Message, 4096)
+	go flush(hostname)
+	log.Printf("Starting stats collector [%s] on [%s]\n", prefix, hostname)
+}
+
+func flush(hostname string) {
+	if !enabled {
+		return
+	}
+
+	var client Client
+	var err error
+
+Connect:
+	// Initializes a statsite client based on the toml config file
+	// Returns a statsite.Client and an error
+	client, err = NewClient(hostname)
+	if err != nil {
+		log.Println("Error connecting to statsite")
+		goto Wait
+	}
+
+	for {
+		select {
+		case msg := <-statQueue:
+			if err := client.Emit(msg); err != nil {
+				log.Println("Failed to write to statsite. Error: ", err)
+				goto Wait
+			}
+		}
+	}
+
+Wait:
+	sleep := time.After(ErrorWaitTime)
+	for {
+		select {
+		case <-statQueue:
+			// Flush any messages sent before re-connecting
+		case <-sleep:
+			goto Connect
+		}
+	}
+}
---
+++ metrics.go
@@ -0,0 +1,80 @@
+package statsite
+
+import (
+	"fmt"
+	"time"
+)
+
+type Metric interface {
+	Emit()
+}
+
+// Timer Metric
+// t := Timer(key)
+// defer t.Emit()
+type timer struct {
+	start time.Time
+	key   string
+}
+
+func Timer(key string) *timer {
+	return &timer{time.Now(), key}
+}
+
+func (t *timer) Emit() {
+	if !enabled {
+		return
+	}
+
+	go func(key string, start, end time.Time) {
+
+		select {
+		case statQueue <- NewTimer(
+			fmt.Sprintf("%s.%s", metricPrefix, key),
+			start,
+			end,
+		):
+		default:
+		}
+	}(t.key, t.start, time.Now())
+}
+
+// Counter Metric
+// t := Timer(key)
+// defer t.Emit()
+type counter struct {
+	key   string
+	count int
+}
+
+func Counter(key string) *counter {
+	return &counter{key, 0}
+}
+
+func CounterAt(key string, i int) *counter {
+	return &counter{key, i}
+}
+
+func (t *counter) Incr() {
+	t.count += 1
+}
+
+func (t *counter) IncrBy(i int) {
+	t.count += i
+}
+
+func (t *counter) Emit() {
+	if !enabled {
+		return
+	}
+
+	go func(key string, count int) {
+		select {
+		case statQueue <- NewCounter(
+			fmt.Sprintf("%s.%s", metricPrefix, key),
+			count,
+		):
+		default:
+		}
+	}(t.key, t.count)
+}
