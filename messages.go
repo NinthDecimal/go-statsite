17fd81c4b58c79822e43db421f7c24bb17a0ad0f
Stephen Hitchner <stephen@kiip.me>
2015-08-26 18:50:52 -0700

    adding message types

---
+++ messages.go
@@ -0,0 +1,80 @@
+package statsite
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
+
+const (
+	TYPE_KEY_VALUE = MessageType("kv") // - Simple Key/Value
+	TYPE_GAUGE     = MessageType("g")  // - Same as kv, compatibility with statsd gauges
+	TYPE_TIMER     = MessageType("ms") // - Timer
+	TYPE_COUNTER   = MessageType("c")  // - Counter
+	TYPE_SET       = MessageType("s")  // - Unique Set
+)
+
+type MessageType string
+
+type Message interface {
+	String() string
+}
+
+type message struct {
+	Key   string
+	Value string
+	Type  MessageType
+}
+
+func (m message) String() string {
+	return fmt.Sprintf("%v:%v|%v\n", m.Key, m.Value, m.Type)
+}
+
+func NewKeyValue(key, value string) Message {
+	return &message{
+		Key:   key,
+		Value: value,
+		Type:  TYPE_KEY_VALUE,
+	}
+}
+
+func NewGauge(key, value string) Message {
+	return &message{
+		Key:   key,
+		Value: value,
+		Type:  TYPE_GAUGE,
+	}
+}
+
+func NewTimer(key string, start, end time.Time) Message {
+	return NewTimerDuration(key, end.Sub(start))
+}
+
+func NewTimerDuration(key string, duration time.Duration) Message {
+	value := int64(duration / time.Millisecond)
+	return &message{
+		Key:   key,
+		Value: strconv.FormatInt(value, 10),
+		Type:  TYPE_TIMER,
+	}
+}
+
+func NewCounter(key string, value int) Message {
+	return NewCounter64(key, int64(value))
+}
+
+func NewCounter64(key string, value int64) Message {
+	return &message{
+		Key:   key,
+		Value: strconv.FormatInt(value, 10),
+		Type:  TYPE_COUNTER,
+	}
+}
+
+func NewSet(key, value string) Message {
+	return &message{
+		Key:   key,
+		Value: value,
+		Type:  TYPE_SET,
+	}
+}
