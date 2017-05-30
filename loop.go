c21a4cd93c601ab7b20bd4911f25a5cae46cf814
James Grady <jlgrady1@gmail.com>
2017-05-30 15:44:26 -0700

    Added shutdown function to cleanly flush and exit

--- loop.go
+++ loop.go
@@ -2,6 +2,7 @@ package statsite
 
 import (
 	"log"
+	"sync"
 	"time"
 )
 
@@ -15,22 +16,28 @@ var ErrorWaitTime time.Duration = time.Duration(10 * time.Second)
 var metricPrefix string
 
 var statQueue chan Message
+var shutdown chan bool
+var wg sync.WaitGroup
 
 func Initialize(hostname string, prefix string) {
 	enabled = true
 	metricPrefix = prefix
 	statQueue = make(chan Message, 4096)
+	shutdown = make(chan bool, 1)
 	go flush(hostname)
 	log.Printf("Starting stats collector [%s] on [%s]\n", prefix, hostname)
 }
 
 func flush(hostname string) {
+	wg.Add(1)
+	defer wg.Done()
 	if !enabled {
 		return
 	}
 
 	var client Client
 	var err error
+	var exit = false
 
 Connect:
 	// Initializes a statsite client based on the toml config file
@@ -46,6 +53,10 @@ Connect:
 		case msg := <-statQueue:
 			if err := client.Emit(msg); err != nil {
 				log.Println("Failed to write to statsite. Error: ", err)
+				if exit {
+					// stop the flusher
+					return
+				}
 				goto Wait
 			}
 		}
@@ -57,8 +68,32 @@ Wait:
 		select {
 		case <-statQueue:
 			// Flush any messages sent before re-connecting
+		case <-shutdown:
+			log.Println("Shutting down go-statsite flusher")
+			exit = true
+			goto Connect
 		case <-sleep:
 			goto Connect
+		}
+	}
+}
+
+// Shutdown is used to cleanly shutdown go-statsite, flushing all metrics before
+// exiting.
+func Shutdown() {
+	if !enabled {
+		return
+	}
+	// Send the shutdown signal to the flusher
+	shutdown <- true
+	for {
+		select {
+		case <-statQueue:
+			// More metrics left to publish
+		default:
+			// No more metrics, disable the flusher and return
+			enabled = false
+			wg.Wait()
 		}
 	}
 }
